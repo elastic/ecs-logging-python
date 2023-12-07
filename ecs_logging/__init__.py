1c8794bfa0328103ceacbce8edca70cb9e325b1b
Eric de Vries <817612+ericdevries@users.noreply.github.com>
2023-12-07 09:10:08 +0100

    fix: undo version update

--- ecs_logging/__init__.py
+++ ecs_logging/__init__.py
@@ -20,7 +20,7 @@ from ._meta import ECS_VERSION
 from ._stdlib import StdlibFormatter
 from ._structlog import StructlogFormatter
 
-__version__ = "2.1.1"
+__version__ = "2.1.0"
 __all__ = [
     "ECS_VERSION",
     "StdlibFormatter",
