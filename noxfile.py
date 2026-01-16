adc387871d9e5cc0b1ee1e64a5a24418f0da31c7
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-01-16 12:28:57 +0100

    Add support for Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -36,7 +36,7 @@ def tests_impl(session):
     )
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14"])
 def test(session):
     tests_impl(session)
 
