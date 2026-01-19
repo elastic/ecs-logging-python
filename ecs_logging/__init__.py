bd1425f5686a2061b355e34644a5b5a2b09af57b
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-01-19 11:23:35 +0100

    Update CHANGELOG and rev to v2.3.0 (#196)

--- ecs_logging/__init__.py
+++ ecs_logging/__init__.py
@@ -20,7 +20,7 @@ from ._meta import ECS_VERSION
 from ._stdlib import StdlibFormatter
 from ._structlog import StructlogFormatter
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 __all__ = [
     "ECS_VERSION",
     "StdlibFormatter",
